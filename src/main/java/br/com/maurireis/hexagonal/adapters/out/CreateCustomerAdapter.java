bb797ca18623d26e7aceb3e87348efa941ac69c2
Mauri Reis <maurigre@gmail.com>
2023-07-05 22:47:00 -0300

    refactor: refactored customer controller and changed insert to create

--- src/main/java/br/com/maurireis/hexagonal/adapters/out/CreateCustomerAdapter.java
+++ src/main/java/br/com/maurireis/hexagonal/adapters/out/CreateCustomerAdapter.java
@@ -3,20 +3,21 @@ package br.com.maurireis.hexagonal.adapters.out;
 import br.com.maurireis.hexagonal.adapters.out.repository.CustomerRepository;
 import br.com.maurireis.hexagonal.adapters.out.repository.mapper.CustomerEntityMapper;
 import br.com.maurireis.hexagonal.application.core.domain.Customer;
-import br.com.maurireis.hexagonal.application.ports.out.InsertCustomerOutputPort;
+import br.com.maurireis.hexagonal.application.ports.out.CreateCustomerOutputPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class InsertCustomerAdapter implements InsertCustomerOutputPort {
+public class CreateCustomerAdapter implements CreateCustomerOutputPort {
 
     private final CustomerRepository customerRepository;
     private final CustomerEntityMapper customerEntityMapper;
 
     @Override
-    public void insert(Customer customer) {
-        var customerEntity = customerEntityMapper.toCustomerEntity(customer);
-        customerRepository.save(customerEntity);
+    public Customer create(Customer customer) {
+        final var customerEntity = customerEntityMapper.toCustomerEntity(customer);
+        final var created = customerRepository.save(customerEntity);
+        return customerEntityMapper.toCustomer(created);
     }
 }
